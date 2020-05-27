3d5020381e9c7a291eaa4e1262caf70927d12ffd
Rjomba2000 <61803381+Rjomba2000@users.noreply.github.com>
2020-05-28 02:05:46 +0300

    Add files via upload

---
+++ lw3/lw3_5.php
@@ -0,0 +1,23 @@
+<?php
+  if (!isset($_GET['email']))
+  {
+	echo 'Не введён email';
+  }
+  else
+  {
+	$email = $_GET['email'];
+	if (file_exists('data/'.$email.'.txt'))
+	{
+      $fPerson = fopen('data/'.$email.'.txt', 'r');
+	  while (!feof($fPerson))
+      {
+        $mytext = fgets($fPerson, 999);
+        echo $mytext."<br />";
+      }
+	}
+	else
+	{
+	  echo 'Человека с таким email не существует';
+	}
+  }
+?>
