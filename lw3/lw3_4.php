9c5f6f64ad0062140ddf41ea07b8b33f6a46fc9c
Rjomba2000 <61803381+Rjomba2000@users.noreply.github.com>
2020-05-28 01:26:19 +0300

    Update lw3_4.php

--- lw3/lw3_4.php
+++ lw3/lw3_4.php
@@ -1,36 +1,36 @@
 <?php
   if (!isset($_GET['email']))
   {
-	echo 'Не введён email';
+    echo 'Не введён email';
   }
   else
   {
-	$email = $_GET['email'];
-	$fPerson = fopen('data/'. $email. '.txt', 'w');
-	$first_name = isset($_GET['first_name']) ? $_GET['first_name'] : null;
-	$last_name = isset($_GET['last_name']) ? $_GET['last_name'] : null;
-	$age = isset($_GET['age']) ? $_GET['age'] : null;
-	fwrite($fPerson, 'Имя: ');
-	$fNameWrite = fwrite($fPerson, $first_name);
+    $email = $_GET['email'];
+    $fPerson = fopen('data/'. $email. '.txt', 'w');
+    $first_name = isset($_GET['first_name']) ? $_GET['first_name'] : null;
+    $last_name = isset($_GET['last_name']) ? $_GET['last_name'] : null;
+    $age = isset($_GET['age']) ? $_GET['age'] : null;
+    fwrite($fPerson, 'Имя: ');
+    $fNameWrite = fwrite($fPerson, $first_name);
     if (!$fNameWrite)
-	{
-	  fwrite($fPerson, 'Данные не введены');
-	}
-	fwrite($fPerson, "\r\n");
-	fwrite($fPerson, 'Фамилия: ');
-	$fNameWrite = fwrite($fPerson, $last_name);
+    {
+      fwrite($fPerson, 'Данные не введены');
+    }
+    fwrite($fPerson, "\r\n");
+    fwrite($fPerson, 'Фамилия: ');
+    $fNameWrite = fwrite($fPerson, $last_name);
     if (!$fNameWrite)
-	{
-	  fwrite($fPerson, 'Данные не введены');
-	}
-	fwrite($fPerson, "\r\n");
-	fwrite($fPerson, 'Возраст: ');
-	$fNameWrite = fwrite($fPerson, $age);
+    {
+      fwrite($fPerson, 'Данные не введены');
+    }
+    fwrite($fPerson, "\r\n");
+    fwrite($fPerson, 'Возраст: ');
+    $fNameWrite = fwrite($fPerson, $age);
     if (!$fNameWrite)
-	{
-	  fwrite($fPerson, 'Данные не введены');
-	}
-	fwrite($fPerson, "\r\n");
-	fclose($fPerson);
+    {
+      fwrite($fPerson, 'Данные не введены');
+    }
+    fwrite($fPerson, "\r\n");
+    fclose($fPerson);
   }
 ?>
