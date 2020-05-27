efcf915f724a195d09fa4e8652aacbdf37e6dad6
Rjomba2000 <61803381+Rjomba2000@users.noreply.github.com>
2020-05-28 01:32:07 +0300

    Update lw3_4.php

--- lw3/lw3_4.php
+++ lw3/lw3_4.php
@@ -5,32 +5,17 @@
   }
   else
   {
-    $email = $_GET['email'];
-    $fPerson = fopen('data/'. $email. '.txt', 'w');
-    $first_name = isset($_GET['first_name']) ? $_GET['first_name'] : null;
-    $last_name = isset($_GET['last_name']) ? $_GET['last_name'] : null;
-    $age = isset($_GET['age']) ? $_GET['age'] : null;
-    fwrite($fPerson, 'Имя: ');
-    $fNameWrite = fwrite($fPerson, $first_name);
-    if (!$fNameWrite)
-    {
-      fwrite($fPerson, 'Данные не введены');
-    }
-    fwrite($fPerson, "\r\n");
-    fwrite($fPerson, 'Фамилия: ');
-    $fNameWrite = fwrite($fPerson, $last_name);
-    if (!$fNameWrite)
-    {
-      fwrite($fPerson, 'Данные не введены');
-    }
-    fwrite($fPerson, "\r\n");
-    fwrite($fPerson, 'Возраст: ');
-    $fNameWrite = fwrite($fPerson, $age);
-    if (!$fNameWrite)
-    {
-      fwrite($fPerson, 'Данные не введены');
-    }
-    fwrite($fPerson, "\r\n");
-    fclose($fPerson);
+	$email = $_GET['email'];
+	$fPerson = fopen('data/'. $email. '.txt', 'w');
+	$first_name = isset($_GET['first_name']) ? $_GET['first_name'] : null;
+	$last_name = isset($_GET['last_name']) ? $_GET['last_name'] : null;
+	$age = isset($_GET['age']) ? $_GET['age'] : null;
+	fwrite($fPerson, 'Имя: ');
+	$fNameWrite = fwrite($fPerson, $first_name. "\r\n");
+	fwrite($fPerson, 'Фамилия: ');
+	$fNameWrite = fwrite($fPerson, $last_name. "\r\n");
+	fwrite($fPerson, 'Возраст: ');
+	$fNameWrite = fwrite($fPerson, $age. "\r\n");
+	fclose($fPerson);
   }
 ?>
