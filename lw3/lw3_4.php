084a408fe762e3a897560a3b8948e4647d7b4709
Rjomba2000 <61803381+Rjomba2000@users.noreply.github.com>
2020-05-28 14:59:55 +0300

    Update lw3_4.php

--- lw3/lw3_4.php
+++ lw3/lw3_4.php
@@ -6,14 +6,14 @@
     else
     {
         $email = $_GET['email'];
-        $fPerson = fopen('data/'. $email.'.txt','w');
+        $fPerson = fopen('data/' . $email . '.txt', 'w');
         $first_name = isset($_GET['first_name']) ? $_GET['first_name'] : null;
         $last_name = isset($_GET['last_name']) ? $_GET['last_name'] : null;
         $age = isset($_GET['age']) ? $_GET['age'] : null;
-        fwrite($fPerson, 'First Name: '.$first_name."\r\n");
-        fwrite($fPerson, 'Last Name: '.$last_name."\r\n");
-        fwrite($fPerson, 'Email: '.$email."\r\n");
-        fwrite($fPerson, 'Age: '.$age."\r\n");
+        fwrite($fPerson, 'First Name: ' . $first_name . "\r\n");
+        fwrite($fPerson, 'Last Name: ' . $last_name . "\r\n");
+        fwrite($fPerson, 'Email: ' . $email . "\r\n");
+        fwrite($fPerson, 'Age: ' . $age . "\r\n");
         fclose($fPerson);
     }
 ?>
